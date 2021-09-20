cf676d6013207d1836413bc87734c0d239341ba4
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2021-09-20 15:56:58 +0200

    #6 Creation of "Distribution Station" Service

---
+++ power-grid/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TotalConsumptionService } from './services/total-consumption/total-consumption.service';
+import { TotalConsumptionController } from './services/total-consumption/total-consumption.controller';
+import { TotalConsumptionController } from './controllers/total-consumption/total-consumption.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, TotalConsumptionController],
+  providers: [AppService, TotalConsumptionService],
+})
+export class AppModule {}
---
+++ power-grid/src/controllers/total-consumption/total-consumption.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { TotalConsumptionService } from 'src/services/total-consumption/total-consumption.service';
+
+@Controller('total-consumption')
+export class TotalConsumptionController {
+    constructor(private readonly totalConsumptionService: TotalConsumptionService) {}
+
+    @Get()
+    getTotalConsumption(): number {
+        return this.totalConsumptionService.getTotalConsumption();
+    }
+}
+
