2cd19c1cf4553d91a1530904376d4fb115397d2b
Mentra20 <yann.martindes@orange.fr>
2021-09-20 20:21:21 +0200

    #14 first try of communication

--- power-grid/src/app.module.ts
+++ power-grid/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TotalConsumptionService } from './services/total-consumption/total-consumption.service';
 import { TotalConsumptionController } from './controllers/total-consumption/total-consumption.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController, TotalConsumptionController],
   providers: [AppService, TotalConsumptionService],
 })
--- power-grid/src/services/total-consumption/total-consumption.service.ts
+++ power-grid/src/services/total-consumption/total-consumption.service.ts
@@ -1,14 +1,36 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom, Observable } from 'rxjs';
+import {AxiosResponse} from "axios";
 
 @Injectable()
 export class TotalConsumptionService {
-    private total_consumption: number;
+    
+    private URL: string
 
-    constructor() {
-        this.total_consumption = 600;
+    constructor(private httpService: HttpService) {
+        this.URL = "http://house:3000/consumption";
+    }
+
+    public async callToAPI(URL: string): Promise<number>{
+        var body = await firstValueFrom(this.httpService.get(URL));
+ 
+        return body.data
     }
 
     getTotalConsumption(): number {
-        return this.total_consumption;
+        var promise = this.callToAPI(this.URL);
+        var cons:number;
+
+        promise.then(
+            (x)=>{
+                cons = x.data;
+            })
+            .catch((error) =>{
+                console.log(error);
+                cons = 0;
+            });
+        return cons;
     }
+    
 }
