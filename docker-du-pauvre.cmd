0e44d1e5ed8ea914c84db52c69824f6b6ad0b40e
Mentra20 <yann.martindes@orange.fr>
2021-09-25 22:23:58 +0200

    Reduce size of command window of script

--- docker-du-pauvre.cmd
+++ docker-du-pauvre.cmd
@@ -1,5 +1,5 @@
-start powershell -command "cd house |npm run start:dev" &
-start powershell -command "cd house-object |npm run start:dev" &
-start powershell -command "cd power-grid |npm run start:dev" &
-start powershell -command "cd supplier |npm run start:dev" &
-start powershell -command "cd scenario |npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house |npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house-object |npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd power-grid |npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd supplier |npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scenario |npm run start:dev " &
