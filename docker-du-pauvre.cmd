e80c4efdc8490f9a8cb516a86fc6672095a7f702
Idrash <tognetti.yohann@outlook.fr>
2021-09-25 21:16:49 +0200

    Suppression de la clock du docker compose

--- docker-du-pauvre.cmd
+++ docker-du-pauvre.cmd
@@ -1,4 +1,3 @@
-start powershell -command "cd clock |npm run start:dev" &
 start powershell -command "cd house |npm run start:dev" &
 start powershell -command "cd house-object |npm run start:dev" &
 start powershell -command "cd power-grid |npm run start:dev" &
