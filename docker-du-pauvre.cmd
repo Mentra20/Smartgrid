e6995e91d20c8ccfc6ce72860a8d6a9763b3e81b
nersissian <tigran.nersissian@etu.unice.fr>
2021-09-30 09:34:43 +0200

    #42 fix docker du pauvre & add npm install command in script

--- docker-du-pauvre.cmd
+++ docker-du-pauvre.cmd
@@ -1,10 +1,10 @@
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house |npm install | npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house-object |npm install | npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd power-grid |npm install | npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd supplier |npm install | npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-verifier | npm install |npm run start:dev " &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd registry-manager | npm install |npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd dataservice | npm install |npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scheduler | npm install |npm run start:dev" &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scenario | npm install |npm run start:dev " &
-start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-manager | npm install |npm run start:dev " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house-object | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd power-grid | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd supplier | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-verifier | npm run start:dev " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd registry-manager | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd dataservice | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scheduler | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scenario | npm run start:dev " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-manager | npm run start:dev " &
---
+++ docker-du-pauvre1stTime.cmd
@@ -0,0 +1,10 @@
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house |npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house-object |npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd power-grid |npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd supplier |npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-verifier | npm install | npm run start:dev " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd registry-manager | npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd dataservice | npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scheduler | npm install | npm run start:dev" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scenario | npm install | npm run start:dev " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-manager | npm install | npm run start:dev " &
