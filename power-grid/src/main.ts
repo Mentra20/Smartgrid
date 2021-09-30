a9da24fa4038d1d99ee686cae69a262251fdbaa6
Idrash <tognetti.yohann@outlook.fr>
2021-09-30 22:22:58 +0200

    #52 Ajout du swagger

--- power-grid/src/main.ts
+++ power-grid/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('House-object')
+    .setDescription('The House-object API description')
+    .setVersion('1.0')
+    .addTag('MVP')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3004);
 }
 bootstrap();
