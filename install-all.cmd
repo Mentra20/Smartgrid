f61c28a36dad9cd3664fdc9a081721614290ebd3
Mentra20 <yann.martindes@orange.fr>
2021-10-02 10:53:11 +0200

    add install all script

---
+++ install-all.cmd
@@ -0,0 +1,10 @@
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house |npm install" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd house-object |npm install" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd supplier |npm install" &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-verifier | npm install " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd registry-manager | npm install " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd dataservice | npm install " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-scheduler | npm install " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd scenario | npm install " &
+start powershell -command "[console]::windowwidth=70; [console]::windowheight=30; cd consumption-manager | npm install " &
+
