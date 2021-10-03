4f211dd11b8c791d0fe93b13defeb6751bec08f2
Idrash <tognetti.yohann@outlook.fr>
2021-10-03 17:17:41 +0200

    #55 #54 oublie

--- run.sh
+++ run.sh
@@ -5,25 +5,25 @@ docker rm scenario2 || true
 docker run -td --network=si5-soa --name scenario2 scenario2
 
 #scenario before/after subscribe
-# echo "=========================================================================================="
-# echo "---------------------------- Scenario : Ajout d'une maison ----------------------------"
-# docker start house1
-# sleep 10
-# docker exec scenario2 sh -c 'IP=house2:3000 node ./scenarioSubscription_beforeSubscribe.js'
-# docker start house2
-# sleep 10
-# docker exec scenario2 sh -c 'IP=house2:3000 node ./scenarioSubscription_afterSubscribe.js'
-# docker stop house1
-# docker stop house2
+echo "=========================================================================================="
+echo "---------------------------- Scenario : Ajout d'une maison ----------------------------"
+docker start house1
+sleep 10
+docker exec scenario2 sh -c 'IP=house2:3000 node ./scenarioSubscription_beforeSubscribe.js'
+docker start house2
+sleep 10
+docker exec scenario2 sh -c 'IP=house2:3000 node ./scenarioSubscription_afterSubscribe.js'
+docker stop house1
+docker stop house2
 
-# #scenario adapt consumption
-# echo "=========================================================================================="
-# echo "---------------------------- Scenario : Adaptation production ----------------------------"
-# docker-compose up --force-recreate -d
-# docker start house1
-# sleep 10
-# docker exec scenario2 sh -c 'IP_PORT=house1:3000 node ./scenarioAdaptConsumption.js'
-# docker stop house1
+#scenario adapt consumption
+echo "=========================================================================================="
+echo "---------------------------- Scenario : Adaptation production ----------------------------"
+docker-compose up --force-recreate -d
+docker start house1
+sleep 10
+docker exec scenario2 sh -c 'IP_PORT=house1:3000 node ./scenarioAdaptConsumption.js'
+docker stop house1
 
 #scenario consumption schedule
 echo "=========================================================================================="
@@ -35,13 +35,13 @@ docker start house3
 docker start house4
 sleep 20
 docker exec scenario2 sh -c 'IP_PORT=house3:3000 node ./scenarioConsumptionSchedule.js'
-# docker stop house1
-# docker stop house2
-# docker stop house3
-# docker stop house4
+docker stop house1
+docker stop house2
+docker stop house3
+docker stop house4
 
-# docker-compose stop
-# docker stop scenario2
+docker-compose stop
+docker stop scenario2
 
 
 
