48eea3bfdd3f92e1c1b41f22a2edd242de8b6d2a
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2021-10-10 17:36:27 +0200

    Modification of Consumption Verifier and Supplier (deletions of methods + modification of URLs and new methods)

--- consumption-verifier/src/services/consumption-peak/consumption-peak.service.ts
+++ consumption-verifier/src/services/consumption-peak/consumption-peak.service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { timeStamp } from 'console';
 
 @Injectable()
 export class ConsumptionPeakService {
-    private URL: string;
+    private URL_consumption_community: string;
+    private housesID: string[];
+    private URL_client_community: string;
+    private URL_adapt_consumption: string;
     private max_production: number;
 
     constructor(private http:HttpService) {
         this.max_production = 5000; //Valeur à modifier
-        this.URL = "http://consumption-manager:3008/community-consumption";
+        this.URL_consumption_community = "http://consumption-manager:3008/get-community-consumption";
+        this.URL_client_community = "http://client-database:3004/client-registry/community";
+        this.URL_adapt_consumption = "http://consumption-scheduler/3002/adapt-consumption";
     }
 
-    verifyIfEnergyPeakExist(date:Date, ID:number): Promise<boolean>{
-        return firstValueFrom(this.http.get(this.URL, {params: {date:date, ID:ID}})).then((body)=>{
+    async verifyIfEnergyPeakExist(date:Date, communityID:number): Promise<boolean>{
+        this.getCommunity(communityID);
+        return await firstValueFrom(this.http.get(this.URL_consumption_community, {params: {date:date, housesID:this.housesID}})).then((body)=>{
 
             var community_consumption: number = body.data;
             
             if(this.checkConsumptionPeak(community_consumption)) {
-                console.log("!! Consumption for community " + ID + " is at a PEAK !!");   
+                console.log("!! Consumption for community " + communityID + " is at a PEAK !!");
+                this.adaptConsumption(communityID);
                 return true;
             }
             else {
-                console.log("Consumption for community " + ID + " is correct.");
+                console.log("Consumption for community " + communityID + " is correct.");
                 return false;
             }
         })
@@ -31,4 +39,22 @@ export class ConsumptionPeakService {
     private checkConsumptionPeak(value:number):boolean {
         return value>=this.max_production;
     }
+
+    private async adaptConsumption(communityID:number) {
+        await this.http.post(this.URL_adapt_consumption, communityID).subscribe( {
+            next: (value) => console.log("Consumption adapted.\n"),
+            error: (error) => console.log(error)
+        })
+    }
+
+    private async getCommunity(ID:number) {
+        return await firstValueFrom(this.http.get(this.URL_client_community), {params: ID}).then((body)=> {
+            var community = body.data;
+
+            for (var house of community) {
+                this.housesID.push(house.id);
+            }
+        })
+    }
 }
+
