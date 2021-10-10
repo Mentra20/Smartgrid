58d8ee8f96622999111bd03d43fe6a3082a03948
Idrash <tognetti.yohann@outlook.fr>
2021-10-10 12:37:39 +0200

    Docker up fonctionnel

--- producer-database/src/app.module.ts
+++ producer-database/src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProducerRegistryService } from './services/producer-registry/producer-registry.service';
 import { ProducerRegistryController } from './controllers/producer-registry/producer-registry.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Producer } from './models/Producer';
 
 @Module({
-  imports: [],
+  imports: [      
+    TypeOrmModule.forFeature([Producer]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'database',
+      port: 5432,
+      username: 'SI5-SOA',
+      password: 'SI5-SOA',
+      database: 'SI5-SOA',
+      entities: [Producer],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ProducerRegistryController],
   providers: [AppService, ProducerRegistryService],
 })
