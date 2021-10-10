3c52cb3c0aa542840f06c18bb285ff246caa1479
Idrash <tognetti.yohann@outlook.fr>
2021-10-10 10:23:40 +0200

    #63 bug module

--- house/src/app.module.ts
+++ house/src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-// import { ConsumptionService } from './services/consumption/consumption.service';
-// import { ConsumptionController } from './controllers/consumption/consumption.controller';
 import { HttpModule } from '@nestjs/axios';
-// import { SubscribeService } from './services/subscribe/subscribe.service';
 import { ConfigModule } from '@nestjs/config';
-// import { ObjectManagerService } from './services/object-manager/object-manager.service';
-// import { ObjectEditorController } from './controllers/object-editor/object-editor.controller';
+
 import { HousesService } from './services/houses/houses.service';
 import { HouseEditorController } from './controllers/house-editor/house-editor.controller';
 import { ManageSchedulObjectController } from './controllers/manage-schedul-object/manage-schedul-object.controller';
@@ -18,17 +12,10 @@ import { ManageSchedulObjectController } from './controllers/manage-schedul-obje
       ConfigModule.forRoot(),
       ],
   controllers: [
-      AppController,
       HouseEditorController,
       ManageSchedulObjectController, 
-    //   ConsumptionController,
-    //   ObjectEditorController,
       ],
   providers: [
-      AppService, 
-    //   ConsumptionService, 
-    //   SubscribeService, 
-    //   ObjectManagerService, 
       HousesService,
       ],
 })
