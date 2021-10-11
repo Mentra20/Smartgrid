04a538a6110ba95bd9e3c751fa1fdf075b69b722
nersissian <tigran.nersissian@etu.unice.fr>
2021-10-11 13:53:25 +0200

    run.sh debug

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ echo "---------------------------- Scenario1 ----------------------------"
 docker exec scenario2 sh -c 'node ./scenarioN1.js'
 docker stop smartrix-21-22-soa-21-22-i_database_1
 docker rm -f -v smartrix-21-22-soa-21-22-i_database_1
+docker-compose build --pull
+COMPOSE_HTTP_TIMEOUT=360 docker-compose up -d
 docker start smartrix-21-22-soa-21-22-i_database_1
 docker exec scenario2 sh -c 'node ./scenarioN2.js'
 
