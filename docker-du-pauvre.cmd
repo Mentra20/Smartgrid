521309db60657ca90aef7e93d2ff60ba76b1a00e
nersissian <tigran.nersissian@etu.unice.fr>
2021-10-16 14:50:56 +0200

    #77 conf docker & delete production-db

--- docker-du-pauvre.cmd
+++ docker-du-pauvre.cmd
@@ -5,10 +5,10 @@ start powershell -command "[console]::windowwidth=100; [console]::windowheight=3
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd consumption-verifier | npm run start:dev" &
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd consumption-adder | npm run start:dev" &
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd consumption-provider | npm run start:dev" &
+start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd global-production-database | npm run start:dev" &
 
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd house | npm run start:dev" &
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd supplier | npm run start:dev" &
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd producer-database | npm run start:dev" &
-start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd production-db | npm run start:dev" &
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd production-provider | npm run start:dev" &
 start powershell -command "[console]::windowwidth=100; [console]::windowheight=30; cd registry-manager | npm run start:dev" &
