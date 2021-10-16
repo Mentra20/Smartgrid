c1d05dc73332a85d95571f45c201d6d4e5418405
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2021-10-16 15:47:30 +0200

    Change supplier by producers and deletion of supplier

--- producers/src/services/production-storage/production-storage.service.spec.ts
+++ producers/src/services/production-storage/production-storage.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import {ProductionServiceStorage} from './production-storage.service'
+import { ProductionStorageService } from './production-storage.service';
 
 describe('ProductionStorageService', () => {
-  let service: ProductionServiceStorage;
+  let service: ProductionStorageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductionServiceStorage],
+      providers: [ProductionStorageService],
     }).compile();
 
-    service = module.get<ProductionServiceStorage>(ProductionServiceStorage);
+    service = module.get<ProductionStorageService>(ProductionStorageService);
   });
 
   it('should be defined', () => {
