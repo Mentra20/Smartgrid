abe19c33e009c92d3b4b48fa2254f2dbf79bcc68
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2021-10-16 15:46:48 +0200

    #78 Creation of production-adapter service and its connection to the Kafka bus

---
+++ production-adapter/src/controllers/production-adapt/production-adapt.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Inject } from '@nestjs/common';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { ProductionAdaptService } from 'src/services/production-adapt/production-adapt.service';
+
+@Controller('production-adapt')
+export class ProductionAdaptController {
+    constructor(private readonly productionAdaptService: ProductionAdaptService,
+        @Inject("PRODUCTION_VERIFIER") private client:ClientKafka) {}
+    
+    async onModuleInit() {
+        this.client.subscribeToResponseOf("production.adapt");
+        await this.client.connect();
+        console.log("Production adapter connected to the Kafka bus.");
+    }
+
+    @MessagePattern("production.adapt")
+    changeProduction(@Payload() productionAdaptMSG:any) {
+        var amountToAdd = productionAdaptMSG;
+        console.log("Producer adapter received the amount of kW producers need to supply : " + amountToAdd + " kW.");
+        this.productionAdaptService.adaptProduction(amountToAdd);
+    }
+
+}
