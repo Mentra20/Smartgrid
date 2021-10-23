e3c7c2430918369961d6da5718abf95c8dfe630a
Valentin Campello <valentin.campello@etu.univ-cotedazur.fr>
2021-10-23 11:04:43 +0200

    #87  corrections

--- autarky/src/main.ts
+++ autarky/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3030);
   app.connectMicroservice({
     transport: Transport.KAFKA,
     options: {
